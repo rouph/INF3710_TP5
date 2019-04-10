54d75f3cbf7550481ef11cf36219d6c50d9f28be
Hulir <hugo.lirette@polymtl.ca>
2019-04-10 16:37:27 -0400

    B: merge bugs

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-
 import { AppComponent } from "./app.component";
 import { CliniqueVetComponent } from "./clinique-vet/clinique-vet.component";
-
 import { AnimalComponent } from "./animal/animal.component";
-<<<<<<< HEAD
 import { TraitementComponent } from "./traitement/traitement.component";
-=======
 import { EmployeComponent } from "./employe/employe.component";
 import { ProprioAnimalComponent } from "./proprio-animal/proprio-animal.component";
->>>>>>> ba09867efdcadb77cd1104232654824add889ca6
 
 const routes: Routes = [
   { path: "app", component: AppComponent },
