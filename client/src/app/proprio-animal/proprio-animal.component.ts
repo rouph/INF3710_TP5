5478a724d9de7023f21dfe042de6904f8555bff3
Hulir <hugo.lirette@polymtl.ca>
2019-04-10 15:14:55 -0400

    D: let insert fonctionne pour proprio (HL)

--- client/src/app/proprio-animal/proprio-animal.component.ts
+++ client/src/app/proprio-animal/proprio-animal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CommunicationService } from '../communication.service';
+import { Proprio } from '../../../../common/tables/Proprio';
 
 @Component({
   selector: 'app-proprio-animal',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProprioAnimalComponent implements OnInit {
 
-  constructor() { }
+  public constructor(private communicationService: CommunicationService) { }
+  public proprios: Proprio[] = [];
+
+
+  public duplicateError: boolean = false;
+
 
   ngOnInit() {
   }
+  public getCliniques(): void {
+    this.communicationService.getCliniques().subscribe((proprios: Proprio[]) => {
+        this.proprios = proprios;
+    });
+}
+  public insertProprio(noProprietaire: string,nom:string, adresse: string, telNumber: string): void {
+    const proprio: any = {
+        "noProprietaire" : noProprietaire,
+        "nom" : nom,
+        "adresse" : adresse,
+        "telNumber" : telNumber,
+    };
+    this.communicationService.inserProprio(proprio).subscribe((res: number) => {
+        if (res > 0) {
+            this.communicationService.filter("update");
+        }
+        this.duplicateError = (res === -1);
+    });
+  }
+
 
 }
